fbaa4caed4c6d1e7173f124ca0a30d712e1a0c54
yanjiangwx <yanjiang@chinasofti.com>
2025-04-19 18:22:45 +0800

    学习gradle系列 https://juejin.cn/post/6937208620337610766#heading-18

--- traceplugin/src/main/java/com/example/asmplugin/MyMethodVisitor.java
+++ traceplugin/src/main/java/com/example/asmplugin/MyMethodVisitor.java
@@ -17,10 +17,10 @@ class MyMethodVisitor extends AdviceAdapter {
 
     @Override
     public void visitMethodInsn(int opcode, String owner, String methodName, String descriptor, boolean isInterface) {
-        System.out.println("Visiting method: " + owner + "." + methodName + descriptor);
+//        System.out.println("Visiting method: " + owner + "." + methodName + descriptor);
         // 检测是否调用了 Toast.show 方法
         if (owner.equals("android/widget/Toast") && methodName.equals("show")) {
-            System.out.println("Skipping Toast.show()");
+//            System.out.println("Skipping Toast.show()");
             // 直接跳过 Toast.show() 方法，因为 TraceUtil.showToast 已经包含了 show 逻辑
             // 替换 Toast.show 为 TraceUtil.showToast
             super.visitMethodInsn(
